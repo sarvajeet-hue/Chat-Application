da4c3a8eb891c9980233494e4e41c779ed8b320f
sarvajeet-hue <sarvajeet.1923en1095@kiet.edu>
2025-01-17 14:49:06 +0530

    update backend

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,20 @@
 const express = require("express");
 const { database } = require("./config/config");
 const { router } = require("./routes/router");
+const jwt = require('jsonwebtoken')
+
 const app = express();
 const cors = require("cors");
 
 const http = require("http");
 const server = http.createServer(app);
 const websockets = require("ws");
+const cookieParser = require("cookie-parser");
 const wss = new websockets.Server({ server });
 
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser())
 database();
 
 app.use("/api/v1", router);
@@ -41,11 +45,30 @@ wss.on("connection", (ws , req) => {
 });
 
 
-wss.on('connection' , (connection , req) => {
-  console.log("this is the new connection" ,req.headers)
-  
+wss.on('connection', (ws, req) => {
+  const urlParams = new URLSearchParams(req.url.split('?')[1]);
+  console.log("urlParams" , urlParams)
+  const token = urlParams.get('token');
+  console.log("req.headers" , req.headers)
 
-}) 
+  if (!token) {
+    ws.send(JSON.stringify({ error: 'Authentication token is missing' }));
+    ws.close();
+    return;
+  }
+
+  try {
+    const decoded = jwt.verify(token, 'sarvajeet');
+    const username = decoded.username;
+    
+
+    console.log(`${username} connected`);
+  } catch (error) {
+    console.error('Token verification failed:', error);
+    ws.send(JSON.stringify({ error: 'Invalid token' }));
+    ws.close();
+  }
+});
 
 
 app.get("/", (req, res) => {
