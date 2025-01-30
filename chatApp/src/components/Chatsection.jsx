9569bbfad4c6a4cc588e98edb6e5b3da719ded68
sarvajeet-hue <sarvajeet.1923en1095@kiet.edu>
2025-01-30 12:43:51 +0530

    updated code

--- chatApp/src/components/Chatsection.jsx
+++ chatApp/src/components/Chatsection.jsx
@@ -1,17 +1,19 @@
-import React, { useState, useEffect, useDeferredValue } from "react";
+import React, { useState, useEffect } from "react";
 import message_Image from "../assets/message_send_button.png";
 import { useForm } from "react-hook-form";
 import { FcCallback } from "react-icons/fc";
 import { FcVideoCall } from "react-icons/fc";
 import { SlOptionsVertical } from "react-icons/sl";
 
-import { WEBSOCKETS_URL } from "../PrimaryUrl";
+import { PRIMARY_URL, WEBSOCKETS_URL } from "../PrimaryUrl";
 import { useDispatch, useSelector } from "react-redux";
 import { clearToken } from "../slices/loginSlice";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 export const Chatsection = () => {
   const [message, setMessage] = useState([]);
+  const [registeredUser, setRegisteredUser] = useState([]);
   const { register, handleSubmit, reset } = useForm();
   const [ws, setWs] = useState(null);
   const dispatch = useDispatch();
@@ -24,11 +26,9 @@ export const Chatsection = () => {
   };
 
   async function sendMessage(data) {
-
     const message = data.message.trim();
-    
+
     if (message) {
-      
       const newMessage = { type: "sent", content: message };
       setMessage((prev) => {
         return [...prev, newMessage];
@@ -46,8 +46,25 @@ export const Chatsection = () => {
     }
   }
 
+  async function userNames() {
+    try {
+      const response = await axios.get(`${PRIMARY_URL}/allRegisteredUser`);
+
+      setRegisteredUser(response?.data?.data);
+      if (!response) {
+        console.log("response was not fetched");
+      }
+      console.log("response of registed user ", response);
+    } catch (error) {
+      console.log("error in registered User", error);
+    }
+  }
   useEffect(() => {
-    const webSocket = new WebSocket(WEBSOCKETS_URL);
+    userNames();
+  }, []);
+
+  useEffect(() => {
+    const webSocket = new WebSocket(`${WEBSOCKETS_URL}?token=${token}`);
 
     webSocket.onopen = () => {
       console.log("WebSocket connection established");
@@ -82,8 +99,15 @@ export const Chatsection = () => {
       >
         Logout
       </button>
-      <div className="flex w-full pt-5 h-[80%] bg-blue-200 gap-5 p-4  ">
-        <div className="w-[30%] border-black border">Contacts</div>
+      <div className="flex w-full pt-5 h-[80%] bg-blue-200 gap-5 p-4  rounded-lg ">
+        <div className="w-[30%] border-black border rounded-lg">
+          <h1 className="text-center font-serif font-bold text-xl p-3">Registered Users</h1>
+          <div className="flex flex-col gap-3 p-2">
+            {registeredUser.map((data, index) => {
+              return <div className="flex items-center justify-center border p-2" key={index}>{data.username}</div>;
+            })} 
+          </div>
+        </div>
 
         {/* grid grid-rows-[auto_1fr_auto] */}
         <div className="w-[60%] h-full flex flex-col  gap-4  border rounded-lg border-black p-3">
